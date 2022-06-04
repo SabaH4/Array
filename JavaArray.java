f6080fbbbb58376144db29bce90b6387aceb85ec
saba <105594163+SabaH4@users.noreply.github.com>
2022-06-04 15:02:29 +0500

    Add files via upload

---
+++ JavaArray.java
@@ -0,0 +1,13 @@
+class JavaArray
+{
+ public static void main(String[] args)
+{
+   int[] age = {12, 4, 5, 2, 5};
+   System.out.println("Age of kids:");
+   System.out.println("First kid: " + age[0]);
+   System.out.println("Second kid: " + age[1]);
+   System.out.println("Third kid: " + age[2]);
+   System.out.println("Fourth kid " + age[3]);
+   System.out.println("Fifth kid: " + age[4]);
+ }
+}
